8c2ebf4ad5a317376e4f36fa228e8571ecb46915
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2023-03-20 22:27:51 +0700

    commit4

---
+++ Payroll_test/Payroll_test/Controllers/SalaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Payroll_test.Controllers
+{
+    public class SalaryController : Controller
+    {
+        // GET: Salary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
