d35afe9be199dce748927428847f2f445db6fdc0
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2023-03-21 16:10:01 +0700

    Complete GrossSalaryToNetSalary

--- Payroll_test/Payroll_test/Controllers/SalaryController.cs
+++ Payroll_test/Payroll_test/Controllers/SalaryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Payroll_test.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,66 @@ namespace Payroll_test.Controllers
     {
         // GET: Salary
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GrossSalaryToNet()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GrossSalaryToNet(Salary model)
+        {
+            // tien tru 3 loai bao hiem
+            model.premium = Decimal.Multiply(model.premiumSalary, (decimal)0.105) ;
+
+            //so tien bi tinh thue thu nhap neu >11tr
+            model.incomeBeforeTax = model.salary - (decimal)model.premium;
+            if (model.incomeBeforeTax > 11000000)
+            {
+                //thu nhập chịu thuế
+                model.incomeTaxes = model.incomeBeforeTax - 11000000 - (model.numberOfDependents * 4400000);
+                if (model.incomeTaxes > 0)
+                {
+                    if (model.incomeTaxes <= 5000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.05);
+                    }
+                    else if (model.incomeTaxes > 5000000 && model.incomeTaxes <= 10000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.1) - 250000;
+                    }
+                    else if (model.incomeTaxes > 10000000 && model.incomeTaxes <= 18000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.15) - 750000;
+                    }
+                    else if (model.incomeTaxes > 18000000 && model.incomeTaxes <= 32000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.2) - 1650000;
+                    }
+                    else if (model.incomeTaxes > 32000000 && model.incomeTaxes <= 52000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.25) - 3250000;
+                    }
+                    else if (model.incomeTaxes > 52000000 && model.incomeTaxes <= 80000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.3) - 5850000;
+                    }
+                    else if (model.incomeTaxes > 80000000)
+                    {
+                        model.personalIncomeTax = Decimal.Multiply(model.incomeTaxes, (decimal)0.35) - 9850000;
+                    }
+                }
+                else
+                {
+                    model.incomeTaxes = 0;
+                }
+                model.finalSalary = model.salary - (decimal)model.premium - model.personalIncomeTax;
+            }
+            return View(model);
+        }
+
+        public ActionResult NetSalaryToGross()
         {
             return View();
         }
--- Payroll_test/Payroll_test/Models/Salary.cs
+++ Payroll_test/Payroll_test/Models/Salary.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Foolproof;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,39 @@ namespace Payroll_test.Models
 {
     public class Salary
     {
+
+
+        [Required(ErrorMessage = "Mời bạn nhập mức lương đóng bảo hiểm: ")]
+        [Range(0, 999999999, ErrorMessage = "Số tiền phải lớn hơn 0")]
+        public decimal premiumSalary { get; set; }
+
+        [Required(ErrorMessage = "Mời bạn chọn vùng: ")]
+        [Range(1, 4, ErrorMessage = "Vùng nằm từ 1 đến 4")]
+        public decimal area { get; set; }
+
+        [Required(ErrorMessage = "Mời bạn nhập Lương: ")]
+        [Range(0, 999999999, ErrorMessage = "Số tiền phải lớn hơn 0")]
+        //[GreaterThan("premiumSalary", ErrorMessage ="Số tiền phải lớn hơn số tiền tối thiểu vùng")]
+        public decimal salary { get; set; }
+
+        [Required(ErrorMessage = "Mời bạn nhập số người phụ thuộc: ")]
+        [Range(0, 999, ErrorMessage = "Số người phụ thuộc ít nhất là 0")]
+        public int numberOfDependents { get; set; }
+
+        // 10.5% bảo hiểm 
+        public decimal? premium { get; set; }
+
+        // thu nhập trước thuế (sau khi trừ đi 10.5% bảo hiểm)
+        public decimal incomeBeforeTax {get;set;}
+
+        //thu nhập chịu thuế
+        public decimal incomeTaxes { get; set; }
+        //thuế thu nhập cá nhân
+        public decimal personalIncomeTax { get; set; }
+
+        public decimal finalSalary { get; set; }
+
+
+
     }
 }
